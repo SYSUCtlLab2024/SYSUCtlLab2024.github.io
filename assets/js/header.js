5c8a80129d116f9e25d3b61c39deaccdd89091fe
SYSUCtlLab2024 <161570404+SYSUCtlLab2024@users.noreply.github.com>
2024-02-29 10:37:15 +0800

    Add files via upload

---
+++ assets/js/header.js
@@ -0,0 +1,57 @@
+document.write('<div class="znav-container znav" id="znav-container">\
+                    <div class="container">\
+                        <nav class="navbar navbar-toggleable-md">\
+                            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\
+                                <div class="hamburger hamburger--emphatic">\
+                                    <div class="hamburger-box">\
+                                        <div class="hamburger-inner"></div>\
+                                    </div>\
+                                </div>\
+                            </button>\
+                            <a class="navbar-brand overflow-hidden" href="/index.html">\
+   <img src="/assets/images/favicons/fav-white.png" style="margin-bottom: 3px; margin-right: 3px;" width="25">Flow\
+                            </a>\
+                            <div class="collapse navbar-collapse" id="navbarNavDropdown">\
+                                <ul class="navbar-nav ml-auto">\
+                                    <li>\
+                                         <a href="/usingFlow.html"> <span class="icon-Arrow-Right2 fw-300 color-warning" ></span> Get Started</a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="JavaScript:void(0)">Resources</a>\
+                                        <ul class="dropdown dropdown-push-left">\
+                                            <li>\
+                                                <a href="/videos.html">Videos</a>\
+                                            </li>\
+                                            <li>\
+                                                <a href="/gallery/index.html">Blog</a>\
+                                            </li>\
+                                            <li>\
+                                                <a href="/publications.html">Publications</a>\
+                                            </li>\
+                                            <li>\
+                                                <a href="/tutorial.html">Tutorials</a>\
+                                            </li>\
+                                        </ul>\
+                                    </li>\
+                                    <li>\
+                                         <a href="#">Benchmarks</a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="JavaScript:void(0)">About</a>\
+                                        <ul class="dropdown">\
+                                            <li>\
+                                                <a href="/team.html">Team</a>\
+                                            </li>\
+                                            <li>\
+                                                <a href="/collaborations.html">Collaboration</a>\
+                                            </li>\
+                                            <li>\
+                                                <a href="/contact.html">Contact</a>\
+                                            </li>\
+                                        </ul>\
+                                    </li>\
+                                </ul>\
+                            </div>\
+                        </nav>\
+                    </div>\
+            </div>');
