443ae728409f3ff37f15333f85006b1c557c034e
SYSUCtlLab2024 <161570404+SYSUCtlLab2024@users.noreply.github.com>
2024-02-29 15:16:11 +0800

    change footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,16 +1,10 @@
 document.write('<footer class="background-1 color-8 py-3 fs--1 fw-600 font-heading">\
                     <div class="container">\
                         <div class="row align-items-center">\
-                            <div class="col-auto font-1">&copy; 2017-20 Flow Team </div>\
+                            <div class="col-auto font-1">&copy; 2024-2024 Apple Control Lab </div>\
                             <div class="col fs-0 text-right">\
-                                <a class="p-2 color-8"  target="_blank" href="https://twitter.com/BerkeleyMsl">\
-                                    <i class="fa fa-twitter"></i>\
-                                </a>\
-                                <a class="p-2 color-8"  target="_blank" href="mailto:flow.berkeley@gmail.com">\
-                                    <i class="fa fa-envelope"></i>\
-                                </a>\
-                                <a class="p-2 color-8"  target="_blank" href="https://groups.google.com/forum/#!forum/flow-dev">\
-                                    <i class="fa fa-google"></i>\
+                                <a class="p-2 color-8"  target="_blank" href="https://ise.sysu.edu.cn/index.htm">\
+                                    <i class="fa fa-university"></i>\
                                 </a>\
                             </div>\
                         </div>\
