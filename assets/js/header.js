17d8b7e2bd7217b85029b7cb313fd70ea1882917
SYSUCtlLab2024 <161570404+SYSUCtlLab2024@users.noreply.github.com>
2024-03-05 15:52:44 +0800

    update index and header

--- assets/js/header.js
+++ assets/js/header.js
@@ -8,7 +8,7 @@ document.write('<div class="znav-container znav" id="znav-container">\
                                     </div>\
                                 </div>\
                             </button>\
-                            <a class="navbar-brand overflow-hidden" href="/index.html">\
+                            <a class="navbar-brand overflow" href="/index.html">\
    <img src="/assets/images/favicons/logo-white.png" style="margin-bottom: 3px; margin-right: 3px;" width="25">Apple&nbsp;Ctl&nbsp;Lab\
                             </a>\
                             <div class="collapse navbar-collapse" id="navbarNavDropdown">\
