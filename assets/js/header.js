29f26f31a1b9b6d82fd5ba04fbcde116d9e0d889
SYSUCtlLab2024 <161570404+SYSUCtlLab2024@users.noreply.github.com>
2024-07-15 15:21:38 +0800

    update

--- assets/js/header.js
+++ assets/js/header.js
@@ -14,10 +14,10 @@ document.write('<div class="znav-container znav" id="znav-container">\
                             <div class="collapse navbar-collapse" id="navbarNavDropdown">\
                                 <ul class="navbar-nav ml-auto">\
                                     <li>\
-                                         <a href="/usingFlow.html"> <span class="icon-Arrow-Right2 fw-300 color-warning" ></span> Traffic</a>\
+                                         <a href="/traffic.html"> <span class="icon-Arrow-Right2 fw-300 color-warning" ></span> Traffic</a>\
                                     </li>\
                                     <li>\
-                                         <a href="/usingFlow.html"> <span class="icon-Arrow-Right2 fw-300 color-warning" ></span> Robot</a>\
+                                         <a href="/robot.html"> <span class="icon-Arrow-Right2 fw-300 color-warning" ></span> Robot</a>\
                                     </li>\
                                     <li>\
                                         <a href="JavaScript:void(0)">Resources</a>\
@@ -26,7 +26,7 @@ document.write('<div class="znav-container znav" id="znav-container">\
                                                 <a href="/videos.html">Videos</a>\
                                             </li>\
                                             <li>\
-                                                <a href="/gallery/index.html">Blog</a>\
+                                                <a href="/gallery/index.html">Blogs</a>\
                                             </li>\
                                             <li>\
                                                 <a href="/papers.html">papers</a>\
@@ -36,18 +36,18 @@ document.write('<div class="znav-container znav" id="znav-container">\
                                             </li>\
                                         </ul>\
                                     </li>\
-                                    <li>\
-                                         <a href="#">Benchmarks</a>\
-                                    </li>\
+                                    // <li>\
+                                        // <a href="#">Benchmarks</a>\
+                                    // </li>\
                                     <li>\
                                         <a href="JavaScript:void(0)">About</a>\
                                         <ul class="dropdown">\
                                             <li>\
                                                 <a href="/team.html">Team</a>\
                                             </li>\
-                                            <li>\
+                                            // <li>\
                                                 <a href="/collaborations.html">Collaboration</a>\
-                                            </li>\
+                                            // </li>\
                                             <li>\
                                                 <a href="/contact.html">Contact</a>\
                                             </li>\
